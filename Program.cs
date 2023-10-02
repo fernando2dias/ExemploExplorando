31dbe1380de1072d529f16204d4e67effca7eb67
Fernando Dias Motta <fernando3dias@gmail.com>
2023-10-02 16:44:02 -0300

    Section finalized

--- Program.cs
+++ Program.cs
@@ -1,19 +1,15 @@
 ﻿using ExemploExplorando.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Fernando";
-p1.Sobrenome = "Motta";
-
-Pessoa p2 = new Pessoa();
-p2.Nome = "Juliana";
-p2.Sobrenome = "Michelsen";
+Pessoa p1 = new Pessoa(nome: "Fernando", sobrenome: "Motta");
+Pessoa p2 = new Pessoa(nome: "Juliana", sobrenome: "Michelsen");
+Pessoa p3 = new Pessoa(nome: "Neco", sobrenome: "Michelsen");
 
 Curso cursoDeIngles = new Curso();
 cursoDeIngles.Nome = "Inglês";
 cursoDeIngles.Alunos = new List<Pessoa>();
 cursoDeIngles.AdicionarAluno(p1);
 cursoDeIngles.AdicionarAluno(p2);
-
+cursoDeIngles.AdicionarAluno(p3);
 cursoDeIngles.ListarAlunos();
 
 
